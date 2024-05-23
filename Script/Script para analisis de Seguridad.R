74ce4cde17bd8ace8e5be66ccc877ece9c388bdd
Felipe <113157110+rojas435@users.noreply.github.com>
2024-05-23 18:38:33 -0500

    Code completo de analisis de proyeto final

--- Script/Script para analisis de Seguridad.R
+++ Script/Script para analisis de Seguridad.R
@@ -76,17 +76,23 @@ ggplot(robos_genero , aes(x = `Pregunta 8`, y = frec_abs, label = paste0(frec_ab
 
 #5.1 Para la proporción de casos de personas que han sido acosadas en la universidad ICESI.
 
+#Yo lo planteo asi, y abajo el plantemiento de norbey, cualquiera de las dos sirve y DAN LO MISMO
 result <- prop.test(table(datos$`Pregunta 3`), p = 0.4, alternative = "greater", conf.level = 0.8)
 
 result
 
-
+#SOLUCION DE NORBEY
 cantidad_data <- length(`data_acoso_result`)
 exito_acoso <- sum(data_acoso_result)
 
 result1.1 <- prop.test(x=exito_acoso, n = cantidad_data, p=0.4, alternative = "greater", conf.level = 0.8)
 
 result1.1
+
+#Intervalos de confianza
+intervalo_confianza <- prop.test(x=exito_acoso, n = cantidad_data, p=0.4, alternative = "two.sided", conf.level = 0.8)
+
+intervalo_confianza
 
 alpha <- 0.1
 if (result$p.value < alpha) {
@@ -96,7 +102,12 @@ if (result$p.value < alpha) {
 }
 
 
+
+
+
+
 #5.2 Para comparar la proporción de hombres y mujeres que han sido víctimas de inseguridad dentro de la universidad. 
+
 
 masculino <- subset(datos, Genero == "Hombre", select = c(num_casos))
 femenino <- subset(datos, Genero == "Mujer", select = c(num_casos))
@@ -113,6 +124,13 @@ result2 <- prop.test(x=c(exito_M, exito_H), n=c(n_mujeres, n_hombres),
                      alternative="greater", conf.level = 0.8)
 
 result2
+
+#Intervalo de Confianza
+
+intervalo_confianza_2 <- prop.test(x=c(exito_M, exito_H), n=c(n_mujeres, n_hombres),
+                     alternative="two.sided", conf.level = 0.8)
+
+intervalo_confianza_2
   
 alpha <- 0.1
 if (result2$p.value < alpha) {
@@ -126,8 +144,14 @@ if (result2$p.value < alpha) {
 #5.3 Para comparar la cantidad de veces por persona que ha sido vulnerada dentro de la universidad ICESI.
 
 result3 <- t.test(`Pregunta 7`, mu = 2, alternative = "greater", conf.level = 0.8)
-  
 print(result3)
+
+
+#Intervalo de Confianza
+intervalo_confianza_3 <- t.test(`Pregunta 7`, mu = 2, alternative = "two.sided", conf.level = 0.8)
+intervalo_confianza_3
+
+
 
 alpha <- 0.1
 if (result3$p.value < alpha) {
@@ -139,14 +163,16 @@ if (result3$p.value < alpha) {
 
 #5.4 Para comparar el promedio de veces que son víctimas de robo los hombres y el promedio de veces que son víctimas de robo las mujeres.
 
-mujeres <- 35
+#Para que le corran esta, tiene que darle en: "Session/set working directory/" y darle la ruta donde tienen el archivo de excel
+datos1= read.xlsx("Datos filtrados, sin babosadas.xlsx",sheet="Hoja1",
+                 startRow = 1,colNames = T)
 
-hombres <- 60
-
-var.test(x= )
+attach(datos1)
 
 
+result4 <- var.test(x=`Hombres`, y=`Mujeres`, null.value = 1, alternative="two.sided", conf.level=0.8)
 
+result4
 
 alpha <- 0.1
 if (result4$p.value < alpha) {
@@ -155,5 +181,12 @@ if (result4$p.value < alpha) {
   cat("El valor p no es menor que alpha. No se rechaza H0.\n")
 }
 
+t.test(x=Hombres, y=Mujeres, alternative="less",
+       mu=0, paired=FALSE, var.equal=TRUE, conf.level=0.8)
 
+#Norbey me dijo que literal con estos 3 codigos ya queda resuelta la hipotesis 
 
+t.test(x=Hombres, y=Mujeres, alternative="two.sided",
+       mu=0, paired=FALSE, var.equal=TRUE, conf.level=0.8)
+
+#por el -0.41 por debajo del promedio de las mujeres 0.01 por encima de las mujeres en el robo
